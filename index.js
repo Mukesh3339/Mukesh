ae1f453d6222b8824e5e99c9a9897cd53b7eab4f
Mukesh Kumar Boddupalli <10281@arthahyd.com>
2024-01-31 11:53:29 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function Circle(radius) {
-    this.radius =radius;
-    this.draw=function(){
-        console.log('draw');
-    }
-}
-
-Circle.call({}, 1);
-const Circle1 = new Circle(1)
-
-console.log(Circle1);
+let x=10;
+let y=20;
+console.log(x+y);
